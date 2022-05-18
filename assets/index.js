400d3ea81ca9d6ea6fa6ace39d7d3710336286e1
Mauricio Carrillo <mcarrillov183@alumno.uaemex.mx>
2022-05-18 14:17:38 -0500

    Plugins

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from "./mediaplayer.js";
+import AutoPlay from "./plugins/AutoPlay";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({el: video, plugins : [
+    new AutoPlay()]});
 
 const button = document.querySelector("button");
 button.onclick = () => player.togglePlay();
+
--- assets/mediaplayer.js
+++ assets/mediaplayer.js
@@ -1,7 +1,16 @@
 function MediaPlayer(config){
     this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
   }
   
+  MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+      plugin.run();
+    });
+  };
+
   MediaPlayer.prototype.play = function() {
     this.media.play();
   };
@@ -10,12 +19,19 @@ function MediaPlayer(config){
     this.media.pause();
   };
   
-  MediaPlayer.prototype.togglePlay = function(){
-    if(this.media.paused){
-      this.play();
-    } else {
-      this.pause();
-    }
-  };
+ MediaPlayer.prototype.togglePlay = function(){
+   if(this.media.paused){
+     this.play();
+   } else {
+     this.pause();
+   }
+ };
+
+ MediaPlayer.prototype.mute = function(){
+  this.media.muted = true;
+ };
+ MediaPlayer.prototype.unmute = function(){
+  this.media.muted = false;
+ };
 
 export default MediaPlayer;
